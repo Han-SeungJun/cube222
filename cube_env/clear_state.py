16242dc120c63e74e76453e5756e469232a379c5
Han-SeungJun <105290026+Han-SeungJun@users.noreply.github.com>
2022-09-28 16:35:26 +0900

    Create clear_state.py

---
+++ cube_env/clear_state.py
@@ -0,0 +1,38 @@
+import os
+import sys
+# sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname("cube2by2"))))
+# from cube22 import *
+import cube22
+import numpy as np
+
+sample = realCube()
+sample.createCube()
+
+def cube_state_array(arr):
+    state = []
+    for floor_dimension in arr:
+        for piece_dimension in floor_dimension:
+            for position_dimension in piece_dimension:
+                state.append(position_dimension)
+    return state
+
+def _goldlist():
+    gold_state_list = []
+
+    for xy_rotate in range(4):
+        for x_rotate in range(4):
+            sample.xRight()
+            gold_state_list.append(cube_state_array(sample.myCube))
+        sample.yRight()
+
+    for zxzx_rotate in range(2):
+        sample.zRight()
+        for x_rotate in range(4):
+            sample.xRight()
+            gold_state_list.append(cube_state_array(sample.myCube))
+        sample.zRight()
+        sample.xRight()
+        gold_state_list.append(cube_state_array(sample.myCube))
+    gold_state_list.pop(20)
+    
+    return gold_state_list
