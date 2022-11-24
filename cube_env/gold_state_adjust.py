e6d5ad707860654e76f258f09f21a07b49fb9699
Han-SeungJun <105290026+Han-SeungJun@users.noreply.github.com>
2022-11-24 14:18:35 +0900

    Update gold_state_adjust.py

--- cube_env/gold_state_adjust.py
+++ cube_env/gold_state_adjust.py
@@ -3,19 +3,13 @@ from gym.envs.classic_control import rendering
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname("cube2by2"))))
 import cube22
-# from cube22 import *
 import clear_state
 
-gold_cell = clear_state._goldlist()
-
 # This definition determines whether cube is complete.
-def state_judgement(cube_state):
-    present_state = []
-    for dimension_position in cube_state:
-        for dimension_piece in dimension_position:
-            for dimension_floor in dimension_piece:
-                present_state.append(dimension_floor)
+def state_judgement(_object):
+    gold_cell = clear_state._goldlist()
+    present_state = clear_state.cube_state_array(_object.myCube)
                 
     for state_kind_number in gold_cell:
-        if state_kind_number == present_state:
+        if present_state == state_kind_number:
             return True
